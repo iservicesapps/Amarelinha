d26e9cf3611074c7ba9422c983ad8c7b4e79a6da
iservicesapps <mario.cosme@iservices.pt>
2015-06-02 17:11:30 +0100

    teste

--- app/src/main/java/pt/iservices/amarelinha/Splash.java
+++ app/src/main/java/pt/iservices/amarelinha/Splash.java
@@ -1,17 +1,16 @@
 package pt.iservices.amarelinha;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-
-public class MainActivity extends Activity {
+/**
+ * Created by mariocosme on 02/06/15.
+ */
+public class Splash extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splash_activity);
     }
 }
