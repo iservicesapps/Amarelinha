bc910dba7aa5ebd577be23e8b69c04ac460eb299
iservicesapps <mario.cosme@iservices.pt>
2015-06-02 17:45:37 +0100

    teste

--- app/src/main/java/pt/iservices/amarelinha/MainMenuActivity.java
+++ app/src/main/java/pt/iservices/amarelinha/MainMenuActivity.java
@@ -2,6 +2,8 @@ package pt.iservices.amarelinha;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 /**
  * Created by mariocosme on 02/06/15.
@@ -12,5 +14,17 @@ public class MainMenuActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu_activity);
+    }
+
+    public void openMenu(View v) {
+        Log.d("teste", "menu!");
+    }
+
+    public void openCard(View v) {
+        Log.d("teste", "card!");
+    }
+
+    public void openContacts(View v) {
+        Log.d("teste", "contacts!");
     }
 }
