7b622db7be195e227e3eb302c889da5887c12363
iservicesapps <mario.cosme@iservices.pt>
2015-06-16 13:37:46 +0100

    teste

--- app/src/main/java/pt/iservices/amarelinha/MealCard.java
+++ app/src/main/java/pt/iservices/amarelinha/MealCard.java
@@ -1,8 +1,11 @@
 package pt.iservices.amarelinha;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import com.androidquery.AQuery;
@@ -13,6 +16,7 @@ import com.androidquery.AQuery;
 public class MealCard extends Activity {
 
     private AQuery aq;
+    private int currentStamps = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,11 @@ public class MealCard extends Activity {
         setContentView(R.layout.meal_card);
 
         aq = new AQuery(this);
+        SharedPreferences prefs = getSharedPreferences("default", 0);
+        currentStamps = prefs.getInt("currentStamps", 0);
+
+        // TODO: Para apagar
+        currentStamps = 2;
 
         setUpLayout();
     }
@@ -28,5 +37,15 @@ public class MealCard extends Activity {
         Typeface chalkboardBold = Typeface.createFromAsset(getAssets(), "fonts/Chalkboard-Bold.ttf");
         TextView titleTV = (TextView) findViewById(R.id.titleTV);
         titleTV.setTypeface(chalkboardBold);
+
+        setCurrentStamps();
+    }
+
+    private void setCurrentStamps() {
+
+    }
+
+    public void insertCode(View v) {
+        Log.d("teste", "insert Code!");
     }
 }
